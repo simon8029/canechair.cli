e6ac14c75c8ca2298b59a7a7652df443803a6017
simon8029 <simon.lau@msn.com>
2017-12-10 13:52:56 -0500

    reset eslint rules for no-console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 import './index.css';
-
-console.log(`webpack bundle test.`)
---
+++ src/index.spec.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe("Test environment testing", () => {
+  it("should pass", () => {
+    expect(true).to.equal(false);
+  });
+});
