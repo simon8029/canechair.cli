8f974589a9d86269017333b6487532bf7a5223dd
simon8029 <simon.lau@msn.com>
2017-12-26 20:31:59 -0500

    add react-redux support

---
+++ canechair-template/src/App.js
@@ -0,0 +1,6 @@
+const App = () =>
+  <div>
+    <h3>CaneChair is Rocking...</h3>
+  </div>
+
+export default App;
--- canechair-template/src/index.js
+++ canechair-template/src/index.js
@@ -1,34 +1,13 @@
-import './index.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Store from './store';
+import App from './App';
+import StoreInit from './store/store.init';
 
-import { getUsers, deleteUser } from './api/usersApi';
-
-// Populate table of users via API call.
-getUsers().then(result => {
-  let usersBody = "";
-
-  result.forEach(user => {
-    usersBody += `<tr>
-      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-      <td>${user.id}</td>
-      <td>${user.firstName}</td>
-      <td>${user.lastName}</td>
-      <td>${user.email}</td>
-      </tr>`
-  });
-
-  global.document.getElementById('users').innerHTML = usersBody;
-
-  const deleteLinks = global.document.getElementsByClassName('deleteUser');
-
-  // Must use array.from to create a real array from a DOM collection
-  // getElementsByClassname only returns an "array like" object
-  Array.from(deleteLinks, link => {
-    link.onclick = function (event) {
-      const element = event.target;
-      event.preventDefault();
-      deleteUser(element.attributes["data-id"].value);
-      const row = element.parentNode.parentNode;
-      row.parentNode.removeChild(row);
-    };
-  });
-});
+const store = Store(StoreInit);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
