0daea01b7beadc887753ea6581526b45906059af
simon8029 <simon.lau@msn.com>
2018-01-05 10:58:16 -0500

    [0.3.0] - Close issue # 16  - Integrate Typescript

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Change Log
 
+##### [0.3.0] - Close issue # 16
+ - Integrate Typescript
 ##### [0.2.7] - Fix issue #6 #7 #9 #10
 - Add .gitignore file
 - Add sampleReducer
--- canechair-template/.settings/webpack/common.js
+++ canechair-template/.settings/webpack/common.js
@@ -27,6 +27,20 @@ const Settings = WebpackMerge([
     ],
     node: {
       fs: "empty"
+    },
+
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+      "react": "React",
+      "react-dom": "ReactDOM"
     }
   },
   WebpackConfigHelper.transformJavaScript({ include: Paths.src })
